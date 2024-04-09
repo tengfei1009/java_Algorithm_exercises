785282ad1efcee0638e25916c37955ecbaa0f7ee
tengfei1009 <3240288774@qq.com>
2024-04-09 11:19:00 +0800

    Changes

--- src/二叉树的遍历/层序遍历_广度优先搜索.java
+++ src/二叉树的遍历/层序遍历_广度优先搜索.java
@@ -1,5 +1,29 @@
 package 二叉树的遍历;
 
-public class 层序遍历_广度优先搜索 {
+import java.util.*;
 
+public class 层序遍历_广度优先搜索 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        //使用队列来模拟
+        Queue<TreeNode> qu = new LinkedList();
+        qu.offer(root);
+        //当队列中没有元素了，说明这个层序遍历中就没有元素添加到队列，也就是终止了
+        while (!qu.isEmpty()) {
+            //记录当前层节点的个数，控制队列里面弹出的节点数量
+            int len = qu.size();
+            //将每一层的数放入一维数组中保存
+            ArrayList<Integer> cur = new ArrayList();
+            while (len > 0) {
+                TreeNode node = qu.poll();
+                cur.add(node.val);
+                if(node.left!=null) qu.offer(node.left);
+                if(node.right!= null)qu.offer(node.right);
+                len--;
+            }
+            res.add(cur);
+        }
+        return res;
+    }
 }
---
+++ src/二叉树的遍历/层序遍历_深度优先搜索.java
@@ -0,0 +1,7 @@
+package 二叉树的遍历;
+
+public class 层序遍历_深度优先搜索 {
+    public static void main(String[] args) {
+        System.out.println("hello_world");
+    }
+}
